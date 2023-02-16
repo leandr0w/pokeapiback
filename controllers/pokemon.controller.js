88d9ee6f91ca379f8767933587d778e7065df982
leandro wasinger <leowasingerz@gmail.com>
2023-02-15 21:26:47 -0300

    finished

--- controllers/pokemon.controller.js
+++ controllers/pokemon.controller.js
@@ -1,8 +1,17 @@
 const Pokemon = require("../models/pokemon.model")
 
 exports.findPokemons = async(req, res) => {
-   try { res.status(200).json({
-        status: 'success'
+   try {
+        const {count, rows} = await Pokemon.findAndCountAll({
+            attributes: ['id', 'name', 'image'],
+            where: {
+                status: 'available'
+            }
+        })
+       res.status(200).json({
+        status: 'success',
+        count,
+        results: rows,
     })
 } catch (error){
         res.status(500).json({
@@ -13,8 +22,28 @@ exports.findPokemons = async(req, res) => {
 }
 
 exports.findPokemon  = async(req, res) => {
-    try { res.status(200).json({
-        status: 'success'
+    try {
+
+        const { id } = req.params 
+        
+        const pokemon = await Pokemon.findOne({
+            attributes: ['id', 'name', 'image'],
+            where: {
+                id,
+                status: 'available',
+            }
+        })
+
+        if(!pokemon){
+            return res.status(404).json({
+                status: 'error',
+                message: 'Resource not found'
+            })
+        }
+
+        res.status(200).json({
+        status: 'success',
+        pokemon
     })
 } catch (error){
         res.status(500).json({
@@ -29,7 +58,10 @@ exports.createPokemon = async (req, res) => {
         
         const { name, image} = req.body
 
-        const pokemon = await Pokemon.create({name, image})
+        const pokemon = await Pokemon.create({
+            name: name.toLowerCase(), 
+            image
+        })
 
         res.status(200).json({
         status: 'success',
@@ -46,7 +78,28 @@ exports.createPokemon = async (req, res) => {
 }
 
 exports.updatePokemon  = async(req, res) => {
-    try { res.status(200).json({
+    try { 
+        
+        const { id } = req.params
+
+        const { name, image } = req.body
+
+        const pokemon = await Pokemon.findOne({
+            where: {
+                id,
+                status: 'available'
+            }
+        })
+        if(!pokemon) {
+            return res.status(404).json({
+                status: 'error',
+                message: 'Result not found'
+            })
+        }
+
+        await pokemon.update({name, image})
+        
+        res.status(200).json({
         status: 'success'
     })
 } catch (error){
@@ -58,8 +111,30 @@ exports.updatePokemon  = async(req, res) => {
 }
 
 exports.deletePokemon  = async(req, res) => {
-    try { res.status(200).json({
-        status: 'success'
+    try { 
+        
+        const { id } = req.params
+
+
+        const pokemon = await Pokemon.findOne({
+            where: {
+                id,
+                status: 'available',
+            }
+        })
+
+        if(!pokemon){
+            return res.status(404).json({
+                status: 'error',
+                message: 'Result not found'
+            })
+        }
+
+        await pokemon.update({status: 'disabled'})
+        
+        res.status(200).json({
+        status: 'success',
+        message: 'pokemon have removed'
     })
 } catch (error){
         res.status(500).json({
